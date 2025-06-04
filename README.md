e1ea7750a988d594328addac9fd6ac20f5bfa676
Roye <roye@bitovi.com>
2025-06-04 16:17:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# @bitovi/n8n-nodes-utils
+
+This repo contains [N8N](https://n8n.io/) util nodes from Bitovi.
+
+## Installation
+
+- Make sure to allow community nodes with `N8N_COMMUNITY_PACKAGES_ENABLED=true`
+- Once logged in to your N8N web UI, go to `/settings/community-nodes` and type `@bitovi/n8n-nodes-utils`
+
+## Need help or have questions?
+
+Need guidance on leveraging AI agents or N8N for your business? Our [AI Agents workshop](https://hubs.ly/Q02X-9Qq0) will equip you with the knowledge and tools necessary to implement successful and valuable agentic workflows.
+
+## License
+
+[MIT](./LICENSE.md)
---
+++ nodes/Utils/Utils.node.ts
@@ -0,0 +1,133 @@
+import {
+	NodeOperationError,
+	type IExecuteFunctions,
+	type INodeExecutionData,
+	type INodeType,
+	type INodeTypeDescription,
+} from 'n8n-workflow';
+import Ajv from 'ajv';
+
+enum Action {
+	SKIP = 'skip',
+	VALIDATE_SCHEMA = 'validateSchema',
+}
+
+export class Utils implements INodeType {
+	description: INodeTypeDescription = {
+		displayName: 'Utils',
+		icon: 'file:bitovi.svg',
+		name: 'utils',
+		group: ['transform'],
+		version: 1,
+		subtitle: '={{ $parameter["operation"] }}',
+		description: 'Utils Node',
+		defaults: {
+			name: 'Utils',
+		},
+		inputs: ['main'],
+		outputs: ['main'],
+		properties: [
+			{
+				displayName: 'Operation',
+				name: 'operation',
+				type: 'options',
+				noDataExpression: true,
+				options: [
+					{
+						name: 'Skip',
+						value: Action.SKIP,
+					},
+					{
+						name: 'Validate Against Schema',
+						value: Action.VALIDATE_SCHEMA,
+					},
+				],
+				default: 'validateSchema',
+			},
+			{
+				displayName: 'Items',
+				name: 'items',
+				type: 'number',
+				noDataExpression: false,
+				required: true,
+				default: 1,
+				displayOptions: {
+					show: {
+						operation: [Action.SKIP],
+					},
+				},
+			},
+			{
+				displayName: 'Input Schema',
+				name: 'inputSchema',
+				type: 'json',
+				noDataExpression: false,
+				typeOptions: {
+					rows: 10,
+				},
+				required: true,
+				default: `{
+  "type": "object",
+  "properties": {
+    "state": {
+      "type": "string"
+    },
+    "cities": {
+      "type": "array",
+      "items": {
+        "type": "string"
+      }
+    }
+  }
+}`,
+				displayOptions: {
+					show: {
+						operation: [Action.VALIDATE_SCHEMA],
+					},
+				},
+			},
+		],
+	};
+
+	async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
+		const items = this.getInputData();
+
+		const returnData: INodeExecutionData[] = [];
+
+		const operation = this.getNodeParameter('operation', 0) as Action;
+
+		if (operation === Action.SKIP) {
+			const skip = this.getNodeParameter('items', 0) as number;
+
+			return [items.splice(skip)];
+		}
+
+		for (let i = 0; i < items.length; i++) {
+			switch (operation) {
+				case Action.VALIDATE_SCHEMA: {
+					const schema = JSON.parse(this.getNodeParameter('inputSchema', i) as string);
+
+					const ajv = new Ajv();
+					const validate = ajv.compile(schema);
+					const valid = validate(items[i].json);
+
+					if (!valid) {
+						throw new NodeOperationError(
+							this.getNode(),
+							`Input data does not match schema: ${ajv.errorsText(validate.errors)}`,
+						);
+					}
+
+					returnData.push(items[i]);
+
+					break;
+				}
+				default: {
+					// Do nothing
+				}
+			}
+		}
+
+		return [returnData];
+	}
+}
